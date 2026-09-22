1ad0555626c7de144b74409c6af2f8ddf96cfb2f
mrrobot2524 <alenjon2594@gmail.com>
2026-09-22 22:24:43 +0500

    fix one error

--- Marketplace.Api/Program.cs
+++ Marketplace.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json.Serialization;
 using HealthChecks.UI.Client;
 using Marketplace.Api.Exceptions;
 using Marketplace.Api.Options;
@@ -15,7 +16,11 @@ using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
